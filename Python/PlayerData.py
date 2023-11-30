78635d96058aaee6c889026896a965e754f477a2
Count X <marklinn@metropolia.fi>
2023-11-30 19:40:44 +0200

    Added PlayerData python file

---
+++ Python/PlayerData.py
@@ -0,0 +1,17 @@
+import json
+
+# Structure of the player json file
+# {
+# "name" : "Name of player",
+# "Fishing Pole" : {"name" : "name of fishing pole",
+#                   "condition" : poleCondition (as float(decimal) 0.0 - 1.0)
+#                   "stringAmount" : amount of string left}
+# "Bait" : "Name of bait",
+# "String" : {"name" : "name of string being used",
+#             "breakPercent" : likelihood breaking out of 100 so 5% = 5},
+# "Luck" : as "Hellish", "Bad", "Neutral", "Good", "Heavenly" (Not complete idea but may add some interest to the game)
+#
+# Leaving room for potential additions
+#
+#
+#}
